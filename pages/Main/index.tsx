8930698c3a2907869e5e8c41f5cf4c39ab669c8b
Gabriel <knuckles4x@gmail.com>
2021-08-15 13:59:36 -0300

    lottie nurse animation

--- pages/Main/index.tsx
+++ pages/Main/index.tsx
@@ -1,12 +1,34 @@
 import { Content } from "./styles";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Lottie from "react-lottie";
+import nurse from "../../lotties/super-nurse-animation.json";
 
 export default function Main() {
+  const { height } = useWindowSize();
+
+  function options(data) {
+    return {
+      loop: true,
+      autoplay: true,
+      animationData: data,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+  }
+
   return (
     <Content>
-      <h1>
-        <span>Hoje Rosenda está no </span>
-        <strong>Cachoeirinha</strong>
-      </h1>
+      <h2>Hoje Rosenda está no</h2>
+      <h1>Cachoeirinha</h1>
+      <Lottie
+        options={options(nurse)}
+        height={height / 2.5}
+        width={height / 2.5}
+        isStopped={false}
+        isPaused={false}
+        speed={0.6}
+      />
     </Content>
   );
 }
--- pages/Main/styles.ts
+++ pages/Main/styles.ts
@@ -6,27 +6,24 @@ export const Content = styled.header`
   align-items: center;
   justify-content: center;
   height: 100%;
-  h1 {
+
+  h2 {
     font-size: 64px;
     font-weight: 400;
-    strong {
-      font-size: 80px;
-      font-weight: 700;
-      white-space: nowrap;
-    }
   }
-  h2 {
-    font-weight: 400;
-    margin-top: 32px;
+  h1 {
+    font-size: 80px;
+    font-weight: 700;
+    white-space: nowrap;
+    margin-bottom: 16px;
   }
+
   @media (max-width: 600px) {
-    h1 {
-      display: flex;
-      flex-direction: column;
-      font-size: 40px;
-    }
     h2 {
-      font-size: 18px;
+      font-size: 32px;
+    }
+    h1 {
+      font-size: 64px;
     }
   }
 `;
