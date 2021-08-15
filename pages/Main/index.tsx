543585403309699d66cface9a729f7ff75a12c93
Gabriel <knuckles4x@gmail.com>
2021-08-15 14:34:50 -0300

    remove styles.ts from pages

--- pages/Main/index.tsx
+++ pages/Main/index.tsx
@@ -1,17 +1,42 @@
 import { useEffect, useState } from "react";
 import { differenceInDays } from "date-fns";
-import useWindowSize from "react-use/lib/useWindowSize";
+import styled from "styled-components";
 import Lottie from "react-lottie";
 import nurse from "../../lotties/super-nurse-animation.json";
-import { Content } from "./styles";
+
+const Content = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  h2 {
+    font-size: 56px;
+    font-weight: 400;
+  }
+  h1 {
+    font-size: 80px;
+    font-weight: 700;
+    white-space: nowrap;
+    margin-bottom: 16px;
+  }
+
+  @media (max-width: 600px) {
+    h2 {
+      font-size: 32px;
+    }
+    h1 {
+      font-size: 64px;
+    }
+  }
+`;
 
 export default function Main() {
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
   const [cachoeirinhaFirstDay] = useState(new Date(2021, 7, 14));
   const [days, setDays] = useState(0);
   const [hospital, setHospital] = useState("");
-
-  const { height } = useWindowSize();
 
   function lottieOptions(data) {
     return {
@@ -42,8 +67,8 @@ export default function Main() {
       <h1>{hospital}</h1>
       <Lottie
         options={lottieOptions(nurse)}
-        height={height / 2.5}
-        width={height / 2.5}
+        height={300}
+        width={300}
         isStopped={false}
         isPaused={false}
         speed={0.6}
